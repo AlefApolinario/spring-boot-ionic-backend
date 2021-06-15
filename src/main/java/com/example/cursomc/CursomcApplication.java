a0a50f54872b7c5dafdad46b2464d92eee64b1c2
Alef Apolinario <alefcristiangomes@gmail.com>
2021-06-15 13:52:38 -0300

    Configuração e envio da aplicação para produção

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -1,35 +1,8 @@
 package com.example.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.cursomc.domain.Categoria;
-import com.example.cursomc.domain.Cidade;
-import com.example.cursomc.domain.Cliente;
-import com.example.cursomc.domain.Endereco;
-import com.example.cursomc.domain.Estado;
-import com.example.cursomc.domain.ItemPedido;
-import com.example.cursomc.domain.Pagamento;
-import com.example.cursomc.domain.PagamentoComBoleto;
-import com.example.cursomc.domain.PagamentoComCartao;
-import com.example.cursomc.domain.Pedido;
-import com.example.cursomc.domain.Produto;
-import com.example.cursomc.domain.enums.EstadoPagamento;
-import com.example.cursomc.domain.enums.TipoCliente;
-import com.example.cursomc.repositories.CategoriaRepository;
-import com.example.cursomc.repositories.CidadeRepository;
-import com.example.cursomc.repositories.ClienteRepository;
-import com.example.cursomc.repositories.EnderecoRepository;
-import com.example.cursomc.repositories.EstadoRepository;
-import com.example.cursomc.repositories.ItemPedidoRepository;
-import com.example.cursomc.repositories.PagamentoRepository;
-import com.example.cursomc.repositories.PedidoRepository;
-import com.example.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
